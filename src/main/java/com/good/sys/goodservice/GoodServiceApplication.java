24de6f05e38d68690ac34c57f32678ac4e01af67
sandwich <yuzhoufengg@126.com>
2018-03-07 23:32:40 +0800

    init project

---
+++ src/main/java/com/good/sys/goodservice/GoodServiceApplication.java
@@ -0,0 +1,12 @@
+package com.good.sys.goodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/good/sys/goodservice/GoodServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.good.sys.goodservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoodServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
